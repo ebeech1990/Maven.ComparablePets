1db5029715758e266798b37845608c61ce4e4095
Emma Beech <ebeech@zipcodes-MacBook-Pro.local>
2020-02-26 12:47:08 -0500

    done

--- src/main/java/io/zipcoder/Application.java
+++ src/main/java/io/zipcoder/Application.java
@@ -1,5 +1,60 @@
 package io.zipcoder;
 
 
+import io.zipcoder.pets.Cat;
+import io.zipcoder.pets.Dog;
+import io.zipcoder.pets.Fish;
+import io.zipcoder.pets.Pet;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
 public class Application {
+
+    private static ArrayList<Pet> pets;
+
+    public static void main(String[] args) {
+        Integer numOfPets = getInteger("How many pets do you have?");
+        ArrayList<Pet> pet = new ArrayList<Pet>();
+        pets = pet;
+
+
+        for(int i = 0; i < numOfPets; i++) {
+            String species = getStringInput("What kind of pet?");
+            String name = getStringInput("What is their name?");
+            if(species.equals("cat")) {
+                pets.add(new Cat(name));
+            }
+            else if(species.equals("dog")) {
+                pets.add(new Dog(name));
+            }
+            else{
+                pets.add(new Fish(name));
+            }
+        }
+
+
+        printPets();
+
+    }
+    public static String getStringInput(String prompt){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(prompt);
+        String userInput = scanner.nextLine();
+        return userInput;
+    }
+
+    public static Integer getInteger(String prompt) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(prompt);
+        String userInput = scanner.nextLine();
+        return Integer.parseInt(userInput);
+    }
+    public static void printPets(){
+        Collections.sort(pets);
+        for(Pet p : pets) {
+            System.out.println(p.getName()+ " "+ p.speak());
+        }
+    }
 }
---
+++ src/main/java/io/zipcoder/pets/Cat.java
@@ -0,0 +1,13 @@
+package io.zipcoder.pets;
+
+public class Cat extends Pet {
+
+    public Cat(String name){
+        super(name);
+    }
+
+    public String speak(){
+        return "meow";
+    }
+
+}
